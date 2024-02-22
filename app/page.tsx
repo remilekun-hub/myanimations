a2c47a708c53443da1a70e2c54dad4060aa6fa07
remilekun-hub <atandaremilekun@gmail.com>
2024-02-22 14:20:34 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,176 @@
+"use client";
+import { useState, useRef } from "react";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
+import { AnimatePresence, motion } from "framer-motion";
+import { cn } from "@/src/lib/cn";
+
+type Card = {
+	id: string;
+	content: JSX.Element | React.ReactNode | string;
+	className: string;
+	thumbnail: string;
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [selected, setSelected] = useState<Card | null>(null);
+	const [lastSelected, setLastSelected] = useState<Card | null>(null);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const handleClick = (card: Card) => {
+		setLastSelected(selected);
+		setSelected(card);
+	};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const handleOutsideClick = () => {
+		setLastSelected(selected);
+		setSelected(null);
+	};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const cards: Card[] = [
+		{
+			id: "1",
+			thumbnail:
+				"https://images.pexels.com/photos/568236/pexels-photo-568236.jpeg",
+			className: "col-span-1 md:col-span-2",
+			content: (
+				<div>
+					<h3>adicing elit</h3>
+					<p>minus molestiae aut?</p>
+				</div>
+			),
+		},
+		{
+			id: "2",
+			thumbnail:
+				"https://images.pexels.com/photos/247431/pexels-photo-247431.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+			className: "col-span-1",
+			content: (
+				<div>
+					<h3>Lorem, ipsum.</h3>
+					<p>jfjf kijrwr</p>
+				</div>
+			),
+		},
+		{
+			id: "3",
+			thumbnail:
+				"https://images.pexels.com/photos/460775/pexels-photo-460775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+			className: "col-span-1",
+			content: (
+				<div>
+					<h3>Lorem, ipsum.</h3>
+					<p>jfjf kijrwr</p>
+				</div>
+			),
+		},
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+		{
+			id: "4",
+			thumbnail:
+				"https://images.pexels.com/photos/1198802/pexels-photo-1198802.jpeg?auto=compress&cs=tinysrgb&w=600",
+			className: "col-span-1 md:col-span-2",
+			content: (
+				<div>
+					<h3>Lorem, ipsum.</h3>
+					<p>jfjf kijrwr</p>
+				</div>
+			),
+		},
+	];
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<div className="h-screen py-14">
+			<div className="grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-5 w-full h-full md:h-[800px] p-6">
+				{cards.map((card) => (
+					<div
+						key={card.id}
+						className={twMerge(card.className, "cursor-pointer")}
+					>
+						<motion.div
+							className={twMerge(
+								card.className,
+								"relative overflow-hidden w-full h-full rounded-xl",
+								lastSelected?.id === card.id
+									? "z-40 bg-white rounded-xl h-full w-full"
+									: "bg-white rounded-xl h-full w-full"
+							)}
+							onClick={() => handleClick(card)}
+							layoutId={card.id}
+							transition={{
+								layout: { duration: 0.4, type: "tween" },
+							}}
+						>
+							<BlurImage card={card} />
+						</motion.div>
+
+						<AnimatePresence>
+							{selected?.id === card.id && (
+								<motion.div
+									layoutId={selected.id}
+									className="rounded-lg cursor-pointer absolute inset-0 h-1/2 w-[97vw] md:w-1/2 m-auto z-[100] overflow-hidden"
+								>
+									<Image
+										src={selected.thumbnail}
+										alt="thumbnail"
+										width={500}
+										height={500}
+										className={twMerge(
+											"object-cover object-center absolute inset-0 h-full w-full transition duration-200"
+										)}
+									/>
+									<motion.button
+										className="text-black font-[600] absolute z-[5] top-4 right-4 w-10 h-10 rounded-full flex justify-center items-center bg-white"
+										onClick={() => handleOutsideClick()}
+										initial={{ opacity: 0 }}
+										animate={{ opacity: 1 }}
+										exit={{ opacity: 0 }}
+									>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke-width="1.5"
+											stroke="currentColor"
+											className="w-6 h-6"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="M6 18 18 6M6 6l12 12"
+											/>
+										</svg>
+									</motion.button>
+								</motion.div>
+							)}
+						</AnimatePresence>
+					</div>
+				))}
+				<motion.div
+					className={twMerge(
+						"absolute h-full w-full left-0 top-0 bg-black opacity-0 z-10",
+						selected?.id ? "pointer-events-auto" : "pointer-events-none"
+					)}
+					animate={{ opacity: selected?.id ? 0.35 : 0 }}
+				/>
+			</div>
+		</div>
+	);
 }
+
+const BlurImage = ({ card }: { card: Card }) => {
+	const [loaded, setLoaded] = useState(false);
+
+	return (
+		<Image
+			src={card.thumbnail}
+			alt="thumbnail"
+			width={500}
+			height={500}
+			onLoad={() => setLoaded(true)}
+			className={twMerge(
+				"grayscale-[90%] object-cover object-center absolute inset-0 h-full w-full transition duration-200",
+				loaded ? "blur-none" : "blur-sm"
+			)}
+		/>
+	);
+};
---
+++ app/tangerine/page.tsx
@@ -0,0 +1,230 @@
+"use client";
+import React, { useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import Image from "next/image";
+import insurance from "../../src/assets/insurance_model_2d13568630.png";
+import travel from "../../src/assets/travel_model_9d77c15e4c.png";
+import pension from "../../src/assets/pensions_model_fbe294901d.png";
+import health from "../../src/assets/health_model_98c7dd5e71.png";
+import { useScroll, motion, useMotionValueEvent } from "framer-motion";
+
+export default function Tangerine() {
+	const scrollContainer = useRef<HTMLDivElement>(null);
+
+	const { scrollYProgress, scrollY } = useScroll({
+		target: scrollContainer,
+		offset: ["start start", "end end"],
+	});
+
+	useMotionValueEvent(scrollY, "change", (latest) => {
+		const prev = scrollY.getPrevious();
+		console.log({ latest, prev });
+	});
+
+	type ProductType = {
+		id?: string;
+		content?: React.ReactNode;
+		background?: string;
+		animate?: boolean;
+		wrapper: React.JSX.ElementType;
+	};
+
+	const InsuranceWrapper = ({ p }: { p: ProductType }) => {
+		const [show, setShow] = useState(true);
+		useMotionValueEvent(scrollY, "change", (latest) => {
+			console.log(latest);
+			if (latest && latest > 300) {
+				setShow(false);
+			}
+			if (latest === 0) {
+				setShow(true);
+			}
+			if (latest && latest < 300) {
+				setShow(true);
+			}
+		});
+		return (
+			<Wrapper
+				p={p}
+				className={twMerge(
+					show
+						? "opacity-100 translate-y-0 delay-[300ms] duration-300 ease-in"
+						: "opacity-0 translate-y-[50px] duration-[300ms] ease-out"
+				)}
+			>
+				<div
+					className={`flex flex-col md:flex-row min-h-screen justify-center md:items-center md:justify-between px-6 lg:px-14 gap-x-10 mx-auto max-w-[1200px]`}
+				>
+					<div className="w-full flex flex-col gap-7">
+						<p className="tracking-[5px] text-[12px] font-[400] text-gray-600">
+							LIFE INSURANCE
+						</p>
+						<h1 className="w-full lg:max-w-[350px] text-[40px] font-[600]">
+							Stay one-step ahead of life
+						</h1>
+						<p className="w-full lg:max-w-[480px] leading-[36px] text-[17px] ">
+							Life happens. Plans kick the bucket. So our life insurance
+							plans will always be there to take care of you and the
+							people you cherish most.
+						</p>
+					</div>
+					<div className="w-full">
+						<Image src={insurance} alt="thumbnail" className="w-full" />
+					</div>
+				</div>
+			</Wrapper>
+		);
+	};
+
+	const TravelWrapper = ({ p }: { p: ProductType }) => {
+		const [show, setShow] = useState(false);
+		useMotionValueEvent(scrollY, "change", (latest) => {
+			console.log(latest);
+			if (latest && latest < 300) {
+				setShow(false);
+			}
+			if (latest && latest > 300) {
+				setShow(true);
+			}
+		});
+
+		return (
+			<Wrapper
+				p={p}
+				className={twMerge(
+					show
+						? "opacity-100 translate-y-0 delay-[300ms] duration-300 ease-in"
+						: "opacity-0 translate-y-[50px] duration-[300ms] ease-out"
+				)}
+			>
+				<div
+					className={`flex  flex-col md:flex-row min-h-screen justify-center md:items-center md:justify-between px-6 lg:px-14 gap-x-10 relative max-w-[1200px] mx-auto`}
+				>
+					<div className="w-full flex flex-col gap-7">
+						<p className="tracking-[5px] text-[12px] font-[400] text-gray-600">
+							GENERAL INSURANCE
+						</p>
+						<h1 className="w-full text-[40px] font-[600]">
+							Cover all Angles
+						</h1>
+						<p className="w-full lg:max-w-[480px] leading-[36px] text-[17px] ">
+							General Insurance helps you cover the big things and little
+							things with a broad range of products and services built
+							for your business and personal life.
+						</p>
+					</div>
+					<div className="w-full">
+						<Image src={travel} alt="thumbnail" className="w-full" />
+					</div>
+				</div>
+			</Wrapper>
+		);
+	};
+
+	const products: ProductType[] = [
+		{
+			id: "1",
+			background: `bg-gradient-to-t from-[#f7ffe9] to-[hsla(0,0%,100%,0)] from-[0%] to-[20%]  to-[50%] transition`,
+			wrapper: InsuranceWrapper,
+		},
+		{
+			id: "2",
+			background: `bg-gradient-to-t from-[#e7e7e7] to-[hsla(0,0%,100%,0)]  from-[0%]  to-[20%] to-[50%] transition`,
+			wrapper: TravelWrapper,
+		},
+		// {
+		// 	id: "3",
+		// 	content: (
+		// 		<div className="flex min-h-screen items-center justify-between px-14 gap-x-10">
+		// 			<div className="w-full flex flex-col gap-7">
+		// 				<h1 className="w-full lg:max-w-[380px] text-[45px] font-[600]">
+		// 					Be free to enjoy life in retirement
+		// 				</h1>
+		// 				<p className="w-full lg:max-w-[480px] leading-[36px] text-[17px] ">
+		// 					A retirement savings account is a step in the right
+		// 					direction in securing your future. With this pensions
+		// 					account, you insure your savings and earn returns on your
+		// 					investment in preparation for life after employment.
+		// 				</p>
+		// 			</div>
+		// 			<div className="w-full">
+		// 				<Image src={pension} alt="thumbnail" className="w-full" />
+		// 			</div>
+		// 		</div>
+		// 	),
+		// 	background:
+		// 		"bg-gradient-to-t from-[#fff3e3] to-[hsla(0,0%,100%,0)] from-[0%] to-[20%] to-[50%] transition",
+		// },
+		// {
+		// 	id: "4",
+		// 	content: (
+		// 		<div className="flex min-h-screen items-center justify-between px-14 gap-x-10">
+		// 			<div className="w-full flex flex-col gap-7">
+		// 				<h1 className="w-full lg:max-w-[380px] text-[45px] font-[600]">
+		// 					Access quality health care
+		// 				</h1>
+		// 				<p className="w-full lg:max-w-[480px] leading-[36px] text-[17px] ">
+		// 					Get connected to qualified healthcare practitioners and
+		// 					hospitals near you. Protect you and your loved ones’ health
+		// 					by getting a plan that offers quality medical services
+		// 					whenever you need to and even in an emergency.
+		// 				</p>
+		// 			</div>
+		// 			<div className="w-full">
+		// 				<Image src={health} alt="thumbnail" className="w-full" />
+		// 			</div>
+		// 		</div>
+		// 	),
+		// 	background:
+		// 		"bg-gradient-to-t from-[#f7ffe9] to-[hsla(0,0%,100%,0)] from-[0%] to-[20%] to-[50%] transition",
+		// },
+	];
+
+	return (
+		<>
+			<div
+				className="min-h-[3000px] relative"
+				ref={scrollContainer}
+				id="wrapper"
+			>
+				<div className="sticky top-0 snap-y">
+					{products.map((p) => (
+						<p.wrapper key={p.id} p={p} />
+					))}
+				</div>
+
+				<div className="fixed w-[2px] top-1/2 left-[calc(50vw-620px)] h-[65%] rounded-full -translate-y-1/2 flex flex-col gap-3 justify-evenly bg-gray-300">
+					<div className="bg-white h-[11px] w-full rounded-full z-[3]"></div>
+					<div className="bg-white h-[11px] w-full rounded-full z-[3]"></div>
+					<div className="bg-white h-[11px] w-full rounded-full z-[3]"></div>
+					<motion.span
+						className="absolute bg-[#ff9100] w-full top-0 origin-top h-full"
+						style={{ scaleY: scrollYProgress }}
+					/>
+				</div>
+			</div>
+		</>
+	);
+
+	function Wrapper({
+		children,
+		p,
+		className,
+	}: {
+		children: React.ReactNode;
+		p: ProductType;
+		className: string;
+	}) {
+		return (
+			<motion.div
+				className={twMerge(
+					"min-h-screen top-0 w-full absolute transition",
+					p.background,
+					className
+				)}
+			>
+				{children}
+			</motion.div>
+		);
+	}
+}
