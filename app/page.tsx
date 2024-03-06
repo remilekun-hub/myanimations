865f64cc1b4ded02cfb980cc7915d12bc1438477
remilekun-hub <atandaremilekun@gmail.com>
2024-03-06 15:32:46 +0100

    tangerine page component

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/src/lib/cn";
+import Tangerine from "@/src/component/tangerine";
 
 type Card = {
 	id: string;
@@ -79,98 +80,99 @@ export default function Home() {
 	];
 
 	return (
-		<div className="min-h-screen relative">
-			<div className="grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-5 w-full min-h-screen md:h-[800px] p-6 py-14">
-				{cards.map((card) => (
-					<div
-						key={card.id}
-						className={twMerge(card.className, "cursor-pointer")}
-					>
-						<motion.div
-							className={twMerge(
-								card.className,
-								"relative overflow-hidden w-full h-full rounded-xl",
-								lastSelected?.id === card.id
-									? "z-40 bg-white rounded-xl h-full w-full"
-									: "bg-white rounded-xl h-full w-full"
-							)}
-							onClick={() => handleClick(card)}
-							layoutId={card.id}
-							transition={{
-								layout: { duration: 0.4, type: "tween" },
-							}}
-						>
-							<BlurImage card={card} />
-						</motion.div>
+		// <div className="min-h-screen relative">
+		// 	<div className="grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-5 w-full min-h-screen md:h-[800px] p-6 py-14">
+		// 		{cards.map((card) => (
+		// 			<div
+		// 				key={card.id}
+		// 				className={twMerge(card.className, "cursor-pointer")}
+		// 			>
+		// 				<motion.div
+		// 					className={twMerge(
+		// 						card.className,
+		// 						"relative overflow-hidden w-full h-full rounded-xl",
+		// 						lastSelected?.id === card.id
+		// 							? "z-40 bg-white rounded-xl h-full w-full"
+		// 							: "bg-white rounded-xl h-full w-full"
+		// 					)}
+		// 					onClick={() => handleClick(card)}
+		// 					layoutId={card.id}
+		// 					transition={{
+		// 						layout: { duration: 0.4, type: "tween" },
+		// 					}}
+		// 				>
+		// 					<BlurImage card={card} />
+		// 				</motion.div>
 
-						<AnimatePresence>
-							{selected?.id === card.id && (
-								<motion.div
-									layoutId={selected.id}
-									className="rounded-lg cursor-pointer absolute inset-0 h-1/2 w-[97vw] md:w-1/2 m-auto z-[100] overflow-hidden"
-								>
-									<Image
-										src={selected.thumbnail}
-										alt="thumbnail"
-										width={500}
-										height={500}
-										className={twMerge(
-											"object-cover object-center absolute inset-0 h-full w-full transition duration-200"
-										)}
-									/>
-									<motion.button
-										className="text-black font-[600] absolute z-[5] top-4 right-4 w-10 h-10 rounded-full flex justify-center items-center bg-white"
-										onClick={() => handleOutsideClick()}
-										initial={{ opacity: 0 }}
-										animate={{ opacity: 1 }}
-										exit={{ opacity: 0 }}
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke-width="1.5"
-											stroke="currentColor"
-											className="w-6 h-6"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												d="M6 18 18 6M6 6l12 12"
-											/>
-										</svg>
-									</motion.button>
-								</motion.div>
-							)}
-						</AnimatePresence>
-					</div>
-				))}
-				<motion.div
-					className={twMerge(
-						"absolute h-full w-full inset-0 bg-black opacity-0 z-10",
-						selected?.id ? "pointer-events-auto" : "pointer-events-none"
-					)}
-					animate={{ opacity: selected?.id ? 0.35 : 0 }}
-				/>
-			</div>
-		</div>
+		// 				<AnimatePresence>
+		// 					{selected?.id === card.id && (
+		// 						<motion.div
+		// 							layoutId={selected.id}
+		// 							className="rounded-lg cursor-pointer absolute inset-0 h-1/2 w-[97vw] md:w-1/2 m-auto z-[100] overflow-hidden"
+		// 						>
+		// 							<Image
+		// 								src={selected.thumbnail}
+		// 								alt="thumbnail"
+		// 								width={500}
+		// 								height={500}
+		// 								className={twMerge(
+		// 									"object-cover object-center absolute inset-0 h-full w-full transition duration-200"
+		// 								)}
+		// 							/>
+		// 							<motion.button
+		// 								className="text-black font-[600] absolute z-[5] top-4 right-4 w-10 h-10 rounded-full flex justify-center items-center bg-white"
+		// 								onClick={() => handleOutsideClick()}
+		// 								initial={{ opacity: 0 }}
+		// 								animate={{ opacity: 1 }}
+		// 								exit={{ opacity: 0 }}
+		// 							>
+		// 								<svg
+		// 									xmlns="http://www.w3.org/2000/svg"
+		// 									fill="none"
+		// 									viewBox="0 0 24 24"
+		// 									stroke-width="1.5"
+		// 									stroke="currentColor"
+		// 									className="w-6 h-6"
+		// 								>
+		// 									<path
+		// 										stroke-linecap="round"
+		// 										stroke-linejoin="round"
+		// 										d="M6 18 18 6M6 6l12 12"
+		// 									/>
+		// 								</svg>
+		// 							</motion.button>
+		// 						</motion.div>
+		// 					)}
+		// 				</AnimatePresence>
+		// 			</div>
+		// 		))}
+		// 		<motion.div
+		// 			className={twMerge(
+		// 				"absolute h-full w-full inset-0 bg-black opacity-0 z-10",
+		// 				selected?.id ? "pointer-events-auto" : "pointer-events-none"
+		// 			)}
+		// 			animate={{ opacity: selected?.id ? 0.35 : 0 }}
+		// 		/>
+		// 	</div>
+		// </div>
+		<Tangerine />
 	);
 }
 
-const BlurImage = ({ card }: { card: Card }) => {
-	const [loaded, setLoaded] = useState(false);
+// const BlurImage = ({ card }: { card: Card }) => {
+// 	const [loaded, setLoaded] = useState(false);
 
-	return (
-		<Image
-			src={card.thumbnail}
-			alt="thumbnail"
-			width={500}
-			height={500}
-			onLoad={() => setLoaded(true)}
-			className={twMerge(
-				"grayscale-[90%] object-cover object-center absolute inset-0 h-full w-full transition duration-200",
-				loaded ? "blur-none" : "blur-sm"
-			)}
-		/>
-	);
-};
+// 	return (
+// 		<Image
+// 			src={card.thumbnail}
+// 			alt="thumbnail"
+// 			width={500}
+// 			height={500}
+// 			onLoad={() => setLoaded(true)}
+// 			className={twMerge(
+// 				"grayscale-[90%] object-cover object-center absolute inset-0 h-full w-full transition duration-200",
+// 				loaded ? "blur-none" : "blur-sm"
+// 			)}
+// 		/>
+// 	);
+// };
---
+++ src/component/tangerine.tsx
@@ -0,0 +1,286 @@
+"use client";
+import React, { useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import Image from "next/image";
+import insurance from "../../src/assets/insurance_model_2d13568630.png";
+import travel from "../../src/assets/travel_model_9d77c15e4c.png";
+import pension from "../../src/assets/pensions_model_fbe294901d.png";
+import health from "../../src/assets/health_model_98c7dd5e71.png";
+import { useScroll, motion, useMotionValueEvent } from "framer-motion";
+import { RootScroll } from "@/src/component/rootscroll";
+
+export default function Tangerine() {
+	const scrollContainer = useRef<HTMLDivElement>(null);
+
+	const { scrollYProgress } = useScroll({
+		target: scrollContainer,
+		offset: ["start start", "end end"],
+	});
+
+	useMotionValueEvent(scrollYProgress, "change", (latest) => {
+		console.log({ latest });
+	});
+
+	type ProductType = {
+		id?: string;
+		content?: React.ReactNode;
+		background?: string;
+		animate?: boolean;
+		wrapper: React.JSX.ElementType;
+	};
+
+	const InsuranceWrapper = ({ p }: { p: ProductType }) => {
+		const [show, setShow] = useState(true);
+		useMotionValueEvent(scrollYProgress, "change", (latest) => {
+			const val = latest * 100;
+			if (val && val > 25) {
+				setShow(false);
+			}
+			if (val === 0) {
+				setShow(true);
+			}
+			if (val && val < 25) {
+				setShow(true);
+			}
+		});
+		return (
+			<Wrapper
+				p={p}
+				className={twMerge(
+					show
+						? "opacity-100 translate-y-0 delay-[300ms] duration-300 ease-in"
+						: "opacity-0 translate-y-[50px] duration-[300ms] ease-out"
+				)}
+			>
+				<div
+					className={`flex flex-col md:flex-row min-h-screen justify-center md:items-center md:justify-between px-6 lg:px-14 gap-x-10 mx-auto max-w-[1200px]`}
+				>
+					<div className="w-full flex flex-col gap-7">
+						<p className="tracking-[5px] text-[12px] font-[400] text-gray-600">
+							LIFE INSURANCE
+						</p>
+						<h1 className="w-full lg:max-w-[350px] text-[40px] font-[600]">
+							Stay one-step ahead of life
+						</h1>
+						<p className="w-full lg:max-w-[480px] leading-[36px] text-[17px] ">
+							Life happens. Plans kick the bucket. So our life insurance
+							plans will always be there to take care of you and the
+							people you cherish most.
+						</p>
+					</div>
+					<div className="w-full">
+						<Image src={insurance} alt="thumbnail" className="w-full" />
+					</div>
+				</div>
+			</Wrapper>
+		);
+	};
+
+	const TravelWrapper = ({ p }: { p: ProductType }) => {
+		const [show, setShow] = useState(false);
+		useMotionValueEvent(scrollYProgress, "change", (latest) => {
+			const val = latest * 100;
+			if (val > 25 && val < 50) {
+				setShow(true);
+			} else {
+				setShow(false);
+			}
+		});
+
+		return (
+			<Wrapper
+				p={p}
+				className={twMerge(
+					show
+						? "opacity-100 translate-y-0 delay-[300ms] duration-300 ease-in"
+						: "opacity-0 translate-y-[50px] duration-[300ms] ease-out"
+				)}
+			>
+				<div
+					className={`flex  flex-col md:flex-row min-h-screen justify-center md:items-center md:justify-between px-6 lg:px-14 gap-x-10 relative max-w-[1200px] mx-auto`}
+				>
+					<div className="w-full flex flex-col gap-7">
+						<p className="tracking-[5px] text-[12px] font-[400] text-gray-600">
+							GENERAL INSURANCE
+						</p>
+						<h1 className="w-full text-[40px] font-[600]">
+							Cover all Angles
+						</h1>
+						<p className="w-full lg:max-w-[480px] leading-[36px] text-[17px] ">
+							General Insurance helps you cover the big things and little
+							things with a broad range of products and services built
+							for your business and personal life.
+						</p>
+					</div>
+					<div className="w-full">
+						<Image src={travel} alt="thumbnail" className="w-full" />
+					</div>
+				</div>
+			</Wrapper>
+		);
+	};
+
+	const PensionWrapper = ({ p }: { p: ProductType }) => {
+		const [show, setShow] = useState(false);
+		useMotionValueEvent(scrollYProgress, "change", (latest) => {
+			const val = latest * 100;
+			if (val > 50 && val < 75) {
+				setShow(true);
+			} else {
+				setShow(false);
+			}
+		});
+
+		return (
+			<Wrapper
+				p={p}
+				className={twMerge(
+					show
+						? "opacity-100 translate-y-0 delay-[300ms] duration-300 ease-in"
+						: "opacity-0 translate-y-[50px] duration-[300ms] ease-out"
+				)}
+			>
+				<div
+					className={`flex  flex-col md:flex-row min-h-screen justify-center md:items-center md:justify-between px-6 lg:px-14 gap-x-10 relative max-w-[1200px] mx-auto`}
+				>
+					<div className="w-full flex flex-col gap-7">
+						<p className="tracking-[5px] text-[12px] font-[400] text-gray-600">
+							PENSIONS
+						</p>
+						<h1 className="w-full lg:max-w-[350px] text-[40px] font-[600]">
+							Be free to enjoy life in retirement
+						</h1>
+						<p className="w-full lg:max-w-[480px] leading-[36px] text-[17px] ">
+							A retirement savings account is a step in the right
+							direction in securing your future. With this pensions
+							account, you insure your savings and earn returns on your
+							investment in preparation for life after employment.
+						</p>
+					</div>
+					<div className="w-full">
+						<Image src={pension} alt="thumbnail" className="w-full" />
+					</div>
+				</div>
+			</Wrapper>
+		);
+	};
+	const AccessWrapper = ({ p }: { p: ProductType }) => {
+		const [show, setShow] = useState(false);
+		useMotionValueEvent(scrollYProgress, "change", (latest) => {
+			const val = latest * 100;
+			if (val > 75) {
+				setShow(true);
+			} else {
+				setShow(false);
+			}
+		});
+
+		return (
+			<Wrapper
+				p={p}
+				className={twMerge(
+					show
+						? "opacity-100 translate-y-0 delay-[300ms] duration-300 ease-in"
+						: "opacity-0 translate-y-[50px] duration-[300ms] ease-out"
+				)}
+			>
+				<div
+					className={`flex  flex-col md:flex-row min-h-screen justify-center md:items-center md:justify-between px-6 lg:px-14 gap-x-10 relative max-w-[1200px] mx-auto`}
+				>
+					<div className="w-full flex flex-col gap-7">
+						<p className="tracking-[5px] text-[12px] font-[400] text-gray-600">
+							HEALTH INSURANCE
+						</p>
+						<h1 className="w-full lg:max-w-[380px] text-[45px] font-[600]">
+							Access quality health care
+						</h1>
+						<p className="w-full lg:max-w-[480px] leading-[36px] text-[17px] ">
+							Get connected to qualified healthcare practitioners and
+							hospitals near you. Protect you and your loved ones’ health
+							by getting a plan that offers quality medical services
+							whenever you need to and even in an emergency.
+						</p>
+					</div>
+					<div className="w-full">
+						<Image src={health} alt="thumbnail" className="w-full" />
+					</div>
+				</div>
+			</Wrapper>
+		);
+	};
+
+	const products: ProductType[] = [
+		{
+			id: "1",
+			background: `bg-gradient-to-t from-[#f7ffe9] to-[hsla(0,0%,100%,0)] from-[0%] to-[20%]  to-[50%] transition`,
+			wrapper: InsuranceWrapper,
+		},
+		{
+			id: "2",
+			background: `bg-gradient-to-t from-[#e7e7e7] to-[hsla(0,0%,100%,0)]  from-[0%]  to-[20%] to-[50%] transition`,
+			wrapper: TravelWrapper,
+		},
+		{
+			id: "3",
+			wrapper: PensionWrapper,
+			background:
+				"bg-gradient-to-t from-[#fff3e3] to-[hsla(0,0%,100%,0)] from-[0%] to-[20%] to-[50%] transition",
+		},
+		{
+			id: "4",
+			wrapper: AccessWrapper,
+			background:
+				"bg-gradient-to-t from-[#f7ffe9] to-[hsla(0,0%,100%,0)] from-[0%] to-[20%] to-[50%] transition",
+		},
+	];
+
+	return (
+		<div className="relative">
+			<RootScroll>
+				<div
+					className="min-h-[4000px] relative"
+					ref={scrollContainer}
+					id="wrapper"
+				>
+					<div className="sticky top-0 snap-y">
+						{products.map((p) => (
+							<p.wrapper key={p.id} p={p} />
+						))}
+					</div>
+
+					<div className="fixed w-[2px] top-1/2 left-[calc(50vw-620px)] h-[650px] rounded-full -translate-y-1/2 flex flex-col gap-3 justify-evenly bg-gray-300">
+						<div className="bg-white h-[11px] w-full rounded-full z-[3]"></div>
+						<div className="bg-white h-[11px] w-full rounded-full z-[3]"></div>
+						<div className="bg-white h-[11px] w-full rounded-full z-[3]"></div>
+						<motion.span
+							className="absolute bg-[#ff9100] w-full top-0 origin-top h-full"
+							style={{ scaleY: scrollYProgress }}
+						/>
+					</div>
+				</div>
+			</RootScroll>
+		</div>
+	);
+
+	function Wrapper({
+		children,
+		p,
+		className,
+	}: {
+		children: React.ReactNode;
+		p: ProductType;
+		className: string;
+	}) {
+		return (
+			<motion.div
+				className={twMerge(
+					"min-h-screen top-0 w-full absolute transition",
+					p.background,
+					className
+				)}
+			>
+				{children}
+			</motion.div>
+		);
+	}
+}
